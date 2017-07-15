7af2915e5019e026817d5e0c3232ecacfcdb9a82
kakaniya <myflasky@163.com>
2017-07-15 12:08:22 +0800

    no message

---
+++ endbutton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endbutton : MonoBehaviour {
+
+    private UILabel endlable;
+
+	// Use this for initialization
+	void Awake () {
+        endlable = GameObject.Find("endbuttonlable").GetComponent<UILabel>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickEndButton() {
+        endlable.text = "敌方回合";
+    }
+}
