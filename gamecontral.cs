96b2ea0c4d8afceee57e774e737d28710c687ab6
kakaniya <myflasky@163.com>
2017-07-16 23:54:51 +0800

    no message

---
+++ gamecontral.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamecontral : MonoBehaviour {
+
+    public Transform gamecard1;
+    public Transform gamecard2;
+
+    public GameObject cardpre;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            RandomGenerateCard();
+        }
+	}
+
+    void RandomGenerateCard() {
+        GameObject go = NGUITools.AddChild(this.gameObject, cardpre);
+        go.transform.position = gamecard1.position;
+        iTween.MoveTo(go, gamecard2.position,1f);
+    }
+}
--- hero2craystal.cs
+++ hero2craystal.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hero2craysta : MonoBehaviour {
+public class hero2craystal : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int useableNumber = 1;
+    public int totalNumber = 1;
+    private UILabel label;
+
+    // Use this for initialization
+    void Awake () {
+        label = this.GetComponent<UILabel>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        UpdateShow();
+    }
+
+    void UpdateShow() {
+        label.text = useableNumber + "/" + totalNumber;
+    }
 }
