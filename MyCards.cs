245767a48579365050c1afe74ae3a109ad7855d6
kakaniya <myflasky@163.com>
2017-07-18 23:56:26 +0800

    no message

---
+++ MyCards.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCards : MonoBehaviour {
+
+    public GameObject cardPre;
+    public Transform card01;
+    public Transform card02;
+
+    private float xOffset;
+    private List<GameObject> cards = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        xOffset = card02.position.x - card01.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            GetCard();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LoseCard();
+        }
+    }
+
+    public void GetCard() {
+        GameObject go = NGUITools.AddChild(this.gameObject, cardPre);
+        Vector3 toPosition = card01.position + new Vector3(xOffset, 0, 0) * cards.Count;
+
+        iTween.MoveTo(go, toPosition, 1f);
+        cards.Add(go);
+    }
+
+    public void LoseCard() {
+        int index = Random.Range(0, cards.Count);
+        Destroy(cards[index]);
+        cards.RemoveAt(index);
+
+        for (int i = 0; i < cards.Count; i++) {
+            Vector3 toPosition = card01.position + new Vector3(xOffset, 0, 0) * i;
+
+            iTween.MoveTo(cards[i], toPosition, 0.5f);
+        }
+    }
+}
--- gamecontral.cs
+++ gamecontral.cs
@@ -6,8 +6,15 @@ public class gamecontral : MonoBehaviour {
 
     public Transform gamecard1;
     public Transform gamecard2;
-
+    public string[] cardNames;
     public GameObject cardpre;
+    public float tranformTime;// = Random.Range(0,2.0f);
+    public int transformSpeed = 30;
+
+    private UISprite nowGenerateCard;
+    private float timer = 0;
+    private bool isTransforming = false;
+
 
     // Use this for initialization
     void Start () {
@@ -19,11 +26,26 @@ public class gamecontral : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space)) {
             RandomGenerateCard();
         }
+
+        if (isTransforming) {
+            tranformTime = Random.Range(1.0f, 2.0f);
+            timer += Time.deltaTime;
+            int index = (int)(timer / (1f / transformSpeed));
+            index %= cardNames.Length;
+            nowGenerateCard.spriteName = cardNames[index];
+
+            if (timer > tranformTime) {
+                timer = 0;
+                isTransforming = false;
+            }
+        }
 	}
 
     void RandomGenerateCard() {
         GameObject go = NGUITools.AddChild(this.gameObject, cardpre);
         go.transform.position = gamecard1.position;
+        nowGenerateCard = go.GetComponent<UISprite>();
         iTween.MoveTo(go, gamecard2.position,1f);
+        isTransforming = true;
     }
 }
